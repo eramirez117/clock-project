7f8cc5bbc330545b9eac44f18dec3bc0dfe8300d
Erik <ericklo1094011@gmail.com>
2018-11-07 15:18:23 -0500

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,27 @@
+const clockSection = document.getElementById("clock");
+
+function getTime() {
+  function pad(number) {
+    if (number < 10) {
+      return "0" + number;
+    } else {
+      return number;
+    }
+  }
+
+  const now = new Date();
+
+  const hh = pad(now.getHours());
+  const mm = pad(now.getMinutes());
+  const ss = pad(now.getSeconds());
+
+  return `${hh}:${mm}:${ss}`;
+}
+
+function tickClock() {
+  clockSection.textContent = getTime();
+}
+tickClock();
+setInterval(tickClock, 1000);
+
+//this is a simple clock that displays time with set Interval method
